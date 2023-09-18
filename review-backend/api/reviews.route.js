5318c203421b6cb413b4a91627f9b4490357b9b9
Shivansh <shivanshkumar760@gmail.com>
2023-09-19 01:19:07 +0530

    Controller.js added

--- review-backend/api/reviews.route.js
+++ review-backend/api/reviews.route.js
@@ -1,7 +1,14 @@
 import express from 'express';
-
+import ReviewsCtrl from "./reviews.controller.js";
 const router=express.Router();
 
-router.route("/").get((req, res) => {res.send("hello");});
+// router.route("/").get((req, res) => {res.send("hello");});
+
+router.route("/movie/:id").get(ReviewsCtrl.apiGetReviews);
+router.route("/new").post(ReviewsCtrl.apiPostReview)
+router.route("/:id")
+.get(ReviewsCtrl.apiGetReview)
+.put(ReviewsCtrl.apiUpdateReview)
+.delete(ReviewsCtrl.apiDeleteReview)
 
 export default router;
